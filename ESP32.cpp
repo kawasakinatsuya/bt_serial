593c4241168e6d6d56c147dc8dd79f88d8c585ea
kawasakinatsuya <kawasaki.natsuya.20@shizuoka.ac.jp>
2025-10-06 17:28:51 +0900

    Create ESP32.cpp

---
+++ ESP32.cpp
@@ -0,0 +1,27 @@
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_BT"); 
+  delay(1000);
+  Serial.println("Bluetooth started");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  SerialBT.println("Hello");
+  if (SerialBT.available()) {
+    String msg = SerialBT.readStringUntil('\n');
+    Serial.print("Received via BT: ");
+    Serial.println(msg);
+
+    SerialBT.println("ACK:" + msg);
+  }
+  
+
+  delay(1000);
+
+}
